91ee604fc989bdc6695613d060d8cd904e8b8347
Loukas Papalazarou <loukas.papalazarou.cy@gmail.com>
2023-10-06 21:25:44 +0100

    completed problem 2, need to run tests

--- problem-2/sumplete.py
+++ problem-2/sumplete.py
@@ -1,44 +1,87 @@
-# 90744:6,3,1,7,9,3,9,5,8;1,12,17;9,9,12;2,2,1,2,1,1,1,2,1;90744;1;0;1
-
-# 6,3,1,7,9,3,9,5,8;1,12,17;9,9,12;2,2,1,2,1,1,1,2,1;1;0;1
-
-# [6,3,1,7,9,3,9,5,8];[1,12,17];[9,9,12];[2,2,1,2,1,1,1,2,1];1;0;1
-# [matrix];[row sums];[col sums];[solution matrix];dunno
-
-# 6 3 1 | 1
-# 7 9 3 | 12
-# 9 5 8 |17
-# - - -
-# 9 9 12
-
-# 0 0 1
-# 0 1 1
-# 1 0 1
-
-
-### TERMINOLOGY ###
-#   problem: the entire matrix along with the target sums. For example: 6,3,1,7,9,3,9,5,8;1,12,17;9,9,12
-#   matrix: either the problem matrix or solution matrix. For example: 6,3,1,7,9,3,9,5,8 or 0,0,1,0,1,1,1,0,1
-#   solution: just the solution matrix. For example 0,0,1,0,1,1,1,0,1
-### TERMINOLOGY ###
 import math
 import random
-import time
 
 
-def load_problem(filename: str):
-    pass
+def generate_problem(k: int, max_num: int = 100):
+    """
+    Generate a random problem string.
 
+    Parameters:
+    - k (int): The size of the square matrix.
+    - max_num (int, optional): The maximum value for random integers in the matrix. Default is 100.
 
-def generate_problem(k: int):
-    pass
+    Returns:
+    - str: A problem string containing the matrix and target sums.
+    """
+    matrix = []
+    for _ in range(k):
+        row = [random.randint(1, max_num) for _ in range(k)]
+        matrix.append(row)
+    solution = []
+    for _ in range(k):
+        row = [
+            random.choices([0, 1], weights=[1 / 3, 2 / 3])[0] for _ in range(k)
+        ]  # Use random.choices with weights
+        solution.append(row)
+
+    row_sums = []
+    col_sums = []
+    for i in range(k):
+        row_curr = 0
+        col_curr = 0
+        for j in range(k):
+            row_curr += matrix[i][j] * solution[i][j]
+            col_curr += matrix[j][i] * solution[j][i]
+        row_sums.append(row_curr)
+        col_sums.append(col_curr)
+
+    s = ""
+    matrix = [str(item) for row in matrix for item in row]
+    row_sums = [str(num) for num in row_sums]
+    col_sums = [str(num) for num in col_sums]
+    s += ",".join(matrix) + ";"
+    s += ",".join(row_sums) + ";"
+    s += ",".join(col_sums)
+    return s
 
 
 def print_problem(problem: str):
-    pass
+    """
+    Print a problem in a formatted way.
+
+    Parameters:
+    - problem (str): A formatted problem string containing the matrix and target sums.
+    """
+    args = problem.split(";")
+    row_sums = args[1].split(",")
+    col_sums = args[2].split(",")
+    m = str_to_matrix(args[0])
+
+    # Print the matrix with row sums
+    for i in range(len(m)):
+        for j in range(len(m[0])):
+            print(f"{m[i][j]:2}", end=" ")
+        print("|", row_sums[i])
+
+    # Print the separator line
+    print("-" * (len(m[0]) * 3 + len(row_sums[0]) + 1))
+
+    # Print column sums
+    for j in range(len(col_sums)):
+        print(f"{col_sums[j]:2}", end=" ")
+    print()
 
 
 def generate_random_solution(k: int) -> str:
+    """
+    Generate a random solution.
+
+    Parameters:
+    - k (int): The size of the square matrix.
+
+    Returns:
+    - str: A randomly generated solution string.
+    """
     if k <= 0:
         raise ValueError("k must be greater than 0")
     binary_list = [str(random.randint(0, 1)) for _ in range(k * k)]
@@ -47,11 +90,20 @@ def generate_random_solution(k: int) -> str:
 
 
 def validate_input(input: str) -> int:
+    """
+    Validate the input string and return the size of the square matrix.
+
+    Parameters:
+    - input (str): A formatted problem string containing the matrix and target sums.
+
+    Returns:
+    - int: The size of the square matrix.
+    """
     if not input.__contains__(";"):
         raise ValueError("Input must contain semicolons.")
     args = input.split(";")
     if len(args) != 3:
-        raise ValueError("Input must be divided in 3 sections separated by ';'.")
+        raise ValueError("Input must be divided into 3 sections separated by ';'.")
     input_matrix = args[0]
     input_matrix = input_matrix.split(",")
     k = math.sqrt(len(input_matrix))
@@ -63,6 +115,15 @@ def validate_input(input: str) -> int:
 
 
 def str_to_matrix(input_str: str) -> list[list[int]]:
+    """
+    Convert a string representation of a matrix to a 2D list of integers.
+
+    Parameters:
+    - input_str (str): A string representation of a matrix.
+
+    Returns:
+    - list[list[int]]: A 2D list of integers representing the matrix.
+    """
     flat_matrix = input_str.split(",")
     n = int(len(flat_matrix) ** 0.5)
     if n * n != len(flat_matrix):
@@ -75,6 +136,16 @@ def str_to_matrix(input_str: str) -> list[list[int]]:
 
 
 def fitness_binary(problem: str, candidate: str) -> int:
+    """
+    Calculate the fitness of a candidate solution using a binary fitness function.
+
+    Parameters:
+    - problem (str): A formatted problem string containing the matrix and target sums.
+    - candidate (str): A candidate solution string.
+
+    Returns:
+    - int: The fitness score (1 if the solution is valid, 0 otherwise).
+    """
     # assume fitness matrix format is correct
     args = problem.split(";")
     problem = str_to_matrix(args[0])
@@ -95,6 +166,16 @@ def fitness_binary(problem: str, candidate: str) -> int:
 
 
 def fitness_scalar(problem: str, candidate: str) -> float:
+    """
+    Calculate the fitness of a candidate solution using a scalar fitness function.
+
+    Parameters:
+    - problem (str): A formatted problem string containing the matrix and target sums.
+    - candidate (str): A candidate solution string.
+
+    Returns:
+    - float: The fitness score (a value between 0 and 1).
+    """
     # assume fitness matrix format is correct
     args = problem.split(";")
     problem = str_to_matrix(args[0])
@@ -118,6 +199,13 @@ def fitness_scalar(problem: str, candidate: str) -> float:
 
 
 def mutate(candidate: str, mutation_rate: float):
+    """
+    Apply mutation to a candidate solution.
+
+    Parameters:
+    - candidate (str): A candidate solution string.
+    - mutation_rate (float): The rate of mutation in candidate solutions.
+    """
     candidate = candidate.split(",")
     for i in range(len(candidate)):
         if random.random() < mutation_rate:
@@ -128,6 +216,17 @@ def mutate(candidate: str, mutation_rate: float):
 
 
 def crossover(parent1: str, parent2: str, crossover_point_rate: float) -> (str, str):
+    """
+    Perform crossover between two parent solutions to produce two offspring solutions.
+
+    Parameters:
+    - parent1 (str): The first parent solution string.
+    - parent2 (str): The second parent solution string.
+    - crossover_point_rate (float): The percentage of genes to be taken from parent 1 during crossover.
+
+    Returns:
+    - Tuple[str, str]: Two offspring solution strings.
+    """
     parent1 = parent1.split(",")
     parent2 = parent2.split(",")
     if len(parent1) != len(parent1):
@@ -147,8 +246,9 @@ def solver(
     breeding_rate=0.1,
     crossover_rate=0.1,
     mutation_rate=0.01,
+    verbose=True,
 ):
-    """function(a, b) -> list""" """
+    """
     Solve a binary matrix optimization problem using a genetic algorithm.
 
     Parameters:
@@ -160,13 +260,15 @@ def solver(
     - breeding_rate (float, optional): The percentage of candidates that will be bred in each generation. Default is 0.5.
     - crossover_rate (float, optional): The percentage of the genes that will be taken from parent 1 when performing crossover. Default is 0.5.
     - mutation_rate (float, optional): The rate of mutation in candidate solutions. Default is 0.01.
+    - verbose (bool, optional): Whether to print debug information. Default is True.
 
     Returns:
-    ?
+    - The solution if it is found, otherwise -1
     """
     # k is the detected dimension of the matrix
     k = validate_input(problem)
-    print(f"Input is valid, detected size: {int(k)}")
+    if verbose:
+        print(f"Input is valid, detected size: {int(k)}")
 
     if not 0 <= n_best <= population_size:
         raise ValueError("k_best should be between 0 and K")
@@ -181,7 +283,6 @@ def solver(
     best_fitness = 0
 
     for generation in range(generations):
-        # time.sleep(0.1)
         # Calculate fitness of each candidate solution
         # For each candidate solution in the population,
         # replace the candidate string with a tuple like this: (candidate_str, candidate_fitness)
@@ -195,12 +296,16 @@ def solver(
         for i in range(population_size):
             fitness_val = fitness_fn(population[i])
             if fitness_val == 1:
-                print(f"Solution found: {population[i]}")
+                if verbose:
+                    print("\n")
+                    print_problem(problem)
+                    print(f"Solution found in generation {generation}: {population[i]}")
                 return population[i]
             population[i] = (population[i], fitness_val)
             best_fitness = max(best_fitness, fitness_val)
 
-        print(f"Generation {generation}. Best fitness: {best_fitness}")
+        if verbose:
+            print(f"Generation {generation}. Best fitness: {best_fitness}")
 
         # We now can sort the population based on fitness
         population.sort(key=lambda x: x[1], reverse=True)
@@ -225,9 +330,6 @@ def solver(
         while len(new_population) < population_size:
             new_population.append(generate_random_solution(k))
         population = new_population[:population_size]
-
-
-problem = "1,0,0,0;1,0;1,0"
-problem = "1,7,1,8;8,0;1,7"
-problem = "6,3,1,7,9,3,9,5,8;1,12,17;9,9,12"
-solver(problem, fitness="scalar")
+    if verbose:
+        print("No solution found")
+    return -1
